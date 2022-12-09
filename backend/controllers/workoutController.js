447bc09a19e54d4b1820e8f9f8444141ddc945d3
Youssef Youssef <youssefyoussef28@gmail.com>
2022-12-09 18:42:11 +0200

    Edits in the backend

--- backend/controllers/workoutController.js
+++ backend/controllers/workoutController.js
@@ -37,7 +37,7 @@ const createWorkout = async (req, res) => {
   if (emptyFields.length > 0) {
     return res
       .status(400)
-      .json({ error: "Please fill in all fields!", emptyFields });
+      .json({ error: "Please fill in all fields!!", emptyFields });
   }
 
   // add doc to DB
